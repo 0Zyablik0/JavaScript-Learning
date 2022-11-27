a33367fb75d8907258c2020596ee872e75acea9a
Artem Kryukov <kryukov.aa@phystech.edu>
2022-11-27 20:20:20 +0300

    JavaScript: The Definitive Guide/Chapter 3: Types, Values and Variables/Boolean/

---
+++ JavaScript: The Definitive Guide/Chapter 3: Types, Values and Variables/Boolean/booleans.js
@@ -0,0 +1,20 @@
+console.log(4 === 5)
+console.log(4 <= 5)
+
+
+let a = 4
+let b = 0
+
+if (a === 4) {
+    b += 1
+} else {
+    a = a + 1
+}
+
+console.log(Boolean(undefined))
+console.log(Boolean(null))
+console.log(Boolean(0))
+console.log(Boolean(-0))
+console.log(Boolean(NaN))
+console.log(Boolean(""))
+
