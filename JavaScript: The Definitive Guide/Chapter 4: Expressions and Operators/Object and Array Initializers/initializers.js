6e11a5f17697cec39eec4d71653bb54413369eca
Artem Kryukov <kryukov.aa@phystech.edu>
2022-11-29 20:54:09 +0300

    JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/Object and Array Initializers

---
+++ JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/Object and Array Initializers/initializers.js
@@ -0,0 +1,27 @@
+//Arrays
+
+console.log([])
+console.log([1 + 2, 3 + 4])
+
+//Nested array
+let matrix = [[1, 2, 3], [4, 5, 6]]
+console.log(matrix)
+
+//Array with undefined:
+console.log([1, , , 4])
+
+//Objects
+
+let p = { x: 1, y: 2 + 3 }
+let q = {}
+console.log(p)
+console.log(q)
+
+
+//Nested objects:
+
+let rectangle = {
+    upperLeft: { x: 1, y: 3 },
+    lowerRight: { x: 3, y: 0 }
+};
+console.log(rectangle)
