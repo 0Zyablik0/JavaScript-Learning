56c2cd4685c69cf3b767d76ec538a5559882457e
Artem Kryukov <kryukov.aa@phystech.edu>
2022-11-29 20:47:22 +0300

    JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/ : primary expressions

---
+++ JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/Primary Expressions/primary.js
@@ -0,0 +1,14 @@
+// literal
+
+console.log(1.23)
+console.log("hello")
+console.log(/pattern/)
+
+// reserved words
+
+console.log(null)
+console.log(false)
+console.log(true)
+console.log(this)
+console.log(undefined)
+
