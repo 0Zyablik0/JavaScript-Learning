af8a9e3899d95c9202d88359b9ef25f956622810
Artem Kryukov <kryukov.aa@phystech.edu>
2022-11-30 21:52:12 +0300

    JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/Object Creation Expressions/

--- JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/Property Access Expressions/conditional.js
+++ JavaScript: The Definitive Guide/Chapter 4: Expressions and Operators/Property Access Expressions/conditional.js
@@ -3,4 +3,4 @@ a = { x: { y : 0} }
 console.log(a.x.y)
 console.log(a.z) // a.z.y leads to TypeError
 console.log(a.z?.y)
-console.log(a.z?.y.r)
+console.log(a.z?.y.r) // optional chaining works for the next undefined also, a.x?.y.r throws TypeError
