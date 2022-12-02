bd622fe830b919e6dea574cd497d56a479338cc9
Artem Kryukov <kryukov.aa@phystech.edu>
2022-12-02 18:30:24 +0300

    JavaScript: The Definitive Guide/Chapter 5: Statements/Expression Statement/

---
+++ JavaScript: The Definitive Guide/Chapter 5: Statements/Expression Statement/expression.js
@@ -0,0 +1,18 @@
+let name = "Artem"
+let greeting = "Hello " + name
+console.log(greeting)
+
+let i = 1
+console.log(i*=3)
+
+
+let object = {
+    x: 1,
+    y: 2
+}
+console.log(delete object.x)
+console.log(object)
+
+
+let cx = Math.cos(19)
+console.log(cx)
