69c7fc2b69c3a7accf02c833614885539189ae89
Artem Kryukov <kryukov.aa@phystech.edu>
2022-12-07 22:54:14 +0300

    JavaScript: The Definitive Guide/Chapter 5: Statements/Loops/ while

---
+++ JavaScript: The Definitive Guide/Chapter 5: Statements/Loops/while.js
@@ -0,0 +1,26 @@
+/*
+ while(expression)
+    statement
+*/
+
+let count = 0
+while (count < 5) {
+    console.log(count);
+    count++;
+}
+
+console.log("--------------------------------")
+
+/*
+do
+    statement
+while(expression);
+*/
+
+count = 0
+do {
+    console.log(count);
+    count++;
+}
+while(count < 0)
+
