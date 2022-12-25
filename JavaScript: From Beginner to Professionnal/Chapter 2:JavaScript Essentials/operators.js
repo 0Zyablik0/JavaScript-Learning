5c744936a1edd71755e0c9e3f8c3a7b5235fefdb
Artem Kryukov <kryukov.aa@phystech.edu>
2022-12-25 20:34:06 +0300

    JavaScript: From Beginner to Professionnal/Chapter 2:JavaScript Essentials/operators.js'

---
+++ JavaScript: From Beginner to Professionnal/Chapter 2:JavaScript Essentials/operators.js
@@ -0,0 +1,40 @@
+// Arithmetic operators: Addition
+
+let nr1 = 5;
+let nr2 = 6;
+console.log(nr1 + nr2);
+
+let str1 = 'Hello, ';
+let str2 = 'world';
+console.log(str1 + str2);
+
+// Arithmetic operators: Subtraction
+
+console.log(nr2 - nr1);
+console.log(str1 - nr1);
+
+// Arithmetic operators: Multiplication
+
+console.log(nr1 * nr2);
+console.log(str2 * nr2);
+console.log(nr2 * str2);
+
+// Arithmetic operators: Division
+
+console.log(nr2 / nr1);
+
+// Arithmetic operators: Exponentiation
+console.log(nr2 ** nr1)
+
+// Arithmetic operators: Modulus
+
+console.log(`${nr2} % ${nr1} = ${nr2 % nr1}`);
+
+// Unary operators: increment and decrement
+let i_nr = 0;
+i_nr++;
+console.log("i_nr++ = ", i_nr);
+
+i_nr = 0;
+i_nr--;
+console.log("i_nr-- = ", i_nr);
