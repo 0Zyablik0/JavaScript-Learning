d5aa2551d64c82e941a68a7390d52e2c88392b6f
Artem Kryukov <kryukov.aa@phystech.edu>
2023-01-17 17:46:05 +0500

    Learning Typescript/Chapter 8: Classes/Beginning

---
+++ Learning Typescript/Chapter 8: Classes/example.ts
@@ -0,0 +1,72 @@
+// Class Methods
+
+class Greeter {
+    constructor(greeterName: string) {
+        console.log(`My name is ${greeterName}`);
+
+    }
+
+    greet(name: string) {
+        console.log(`Hello ${name}!`)
+    }
+}
+
+new Greeter("Polite Greeter").greet("world")
+
+// Class Properties
+
+class FieldTrip {
+    destination: string;
+    constructor(destination: string) {
+        this.destination = destination;
+    }
+
+    go() {
+        console.log(`We are going to ${this.destination}`)
+    }
+
+}
+
+new FieldTrip("Moscow").go()
+
+// Function Properties
+
+class WithMethod {
+    method() { console.log("Method is called") }
+}
+
+class WithProperty {
+    property = () => { console.log("Property is called") }
+
+}
+new WithMethod().method()
+new WithProperty().property();
+
+//Initialization Checking
+
+class WithValue {
+    immediate = 0;
+    later: number;
+    maybeUndefined: number | undefined;
+    constructor() {
+        this.later = 0;
+    }
+}
+
+// Optional Properties
+
+class MissingInitializer {
+    property?: number;
+}
+
+// Read-Only Properties;
+class Quote {
+    readonly text: string;
+    constructor(text: string) {
+        this.text = text;
+    };
+
+}
+
+
+// Classes as Types
