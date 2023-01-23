295d9b84a4ab65193525248f34938ba63f8e3431
Artem Kryukov <kryukov.aa@phystech.edu>
2023-01-23 21:09:11 +0500

    Learning Typescript/Chapter 10: Generics/Generic Functions

---
+++ Learning Typescript/Chapter 10: Generics/example.ts
@@ -0,0 +1,33 @@
+// Generic Functions
+
+function identity<T>(value: T): T {
+    return value;
+}
+
+const numeric = identity(10)
+const string = identity('string')
+
+
+const arrowIdentity = <T>(input: T): T => { return input }
+
+let numReturn = arrowIdentity(7);
+
+//Explicit Generic Call Types
+
+
+function logWrapper<Input>(callback: (input: Input) => void) {
+    return (input: Input) => {
+        console.log(input);
+        callback(input);
+    }
+
+};
+
+logWrapper<string>((input) => {console.log(input.length)})
+//Multiple Function Parameters
+
+function makeTuple<First, Second>(first: First, second: Second) {
+    return [first, second] as const;
+}
+
+let tuple = makeTuple(10, "string")
