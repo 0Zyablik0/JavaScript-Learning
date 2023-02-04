ff0245ea2a1c29812d4a17cc88dc1370e3370bdc
Artem Kryukov <kryukov.aa@phystech.edu>
2023-02-04 20:23:47 +0300

    JavaScript: From Beginner to Professionnal/Chapter 8: Built-In JavaScript Methods Parse Numbers

---
+++ JavaScript: From Beginner to Professionnal/Chapter 8: Built-In JavaScript Methods/main.js
@@ -0,0 +1,23 @@
+// Global methods
+
+let x = 7;
+console.log(Number.isNaN(x))
+console.log(isNaN(x))
+
+
+//Parsing Numbers
+
+console.log(parseInt("6"), typeof parseInt("6"))
+console.log(parseInt("7.6"), typeof parseInt("7.6"))
+console.log(parseInt("0b1101010"), typeof parseInt("0b1101010"))
+console.log(parseInt("hello"), typeof parseInt("hello"))
+
+
+// Parse floats
+
+console.log(parseFloat("7.6"), typeof parseFloat("7.6"))
+console.log(parseFloat("7.6.5"), typeof parseFloat("7.6.5"))
+console.log(parseFloat("6"), typeof parseFloat("6"))
+console.log(parseFloat("0b1101010"), typeof parseFloat("0b1101010"))
+console.log(parseFloat("hello"), typeof parseFloat("hello"))
+
